11c07ef1104a13045c2eef51d368992c65c61169
PatricioBrenner <patricio.brenneres@uanl.edu.mx>
2020-11-16 12:57:37 -0600

    Add opción 2

--- conta31.py
+++ conta31.py
@@ -1,27 +1,36 @@
-totald_2009=[]
+from os import system, name
+
+def borrar():
+  if name == "nt":
+    system("cls")
+  else:
+    system("clear")
+
+
 respuestaMenu = 0
 
-print('Bienvenido al Programa de Presupuesto Maestro')
-print('1. Presupuesto de Ventas')
-print('2. Determinación del saldo de Clientes y Flujo de Entradas ')
-print('3. Prespuesto de Producción')
-print('4. Presupuesto de Requerimento de Materiales')
-print('5. Presupuesto de Compra de Materiales')
-print('6. Determinacion del saldo de Proveedores y Flujo de Salidas')
-print('7. Presupuesto de Mano de Obra  Directa')
-print('8. Presupuesto de Gastos Indirectos de Fabricación')
-print('9. Presupuesto de Gastos de Operación')
-print('10. Determinación de Costo Unitario de Productos Terminados')
-print('11. Valuación de Inventarios Finales')
-print('12. Presupuesto Financiero')
-print('13. Salir')
-print(' ')
 
 while respuestaMenu != 13:
+    print('Bienvenido al Programa de Presupuesto Maestro')
+    print('1. Presupuesto de Ventas')
+    print('2. Determinación del saldo de Clientes y Flujo de Entradas ')
+    print('3. Prespuesto de Producción')
+    print('4. Presupuesto de Requerimento de Materiales')
+    print('5. Presupuesto de Compra de Materiales')
+    print('6. Determinacion del saldo de Proveedores y Flujo de Salidas')
+    print('7. Presupuesto de Mano de Obra  Directa')
+    print('8. Presupuesto de Gastos Indirectos de Fabricación')
+    print('9. Presupuesto de Gastos de Operación')
+    print('10. Determinación de Costo Unitario de Productos Terminados')
+    print('11. Valuación de Inventarios Finales')
+    print('12. Presupuesto Financiero')
+    print('13. Salir')
+    print(' ')
+    
     respuestaMenu = int(input('Seleccione una opcion del MENU: '))
     print(' ')
     if respuestaMenu == 1:
-        print('Seleccciono Presupuesto de Ventas')
+        print('Selecccionó Presupuesto de Ventas')
          #ProductoD
         unidades_d=int(input('Ingresa las unidades a vender del producto D en el primer semestre: '))
         preciov_d=int(input("Ingresa el precio venta de las unidades del primer semestre: "))
@@ -59,12 +68,30 @@ while respuestaMenu != 13:
         print(f"\nEl importe total del 2009 del producto Z es de {total_prod_z}")
 
         #Total 2009 3 productos
-        total_3productos = (total_prod_d + total_prod_di + total_prod_z)
-        print(f"\nEl total del 2009 por los 3 productos es de {total_3productos}")
+        ventas_2009 = (total_prod_d + total_prod_di + total_prod_z)
+        print(f"\nEl total del 2009 por los 3 productos es de {ventas_2009}")
+
+        input("Presione enter para continuar...")
+        borrar()
 
     elif respuestaMenu == 2:
-        print('Seleccciono la Determinación del saldo de Clientes y Flujo de Entradas ')
+        print('Selecccionó la Determinación del saldo de Clientes y Flujo de Entradas ')
         print(' ')
+        saldo_clientes_2008 = int(input("\nIngrese el saldo de clientes al 31 de Diciembre de 2008: "))
+        total_clientes_2009 = (saldo_clientes_2008 + ventas_2009)
+        print(f"\nTotal de clientes 2009: {total_clientes_2009}")
+
+        print("\n Entradas de efectivo")
+        por_cobranza_2008 = int(input("\nIngrese cobranza del 2008: "))
+        por_cobranza_2009 = (ventas_2009 * 0.9)
+        total_entradas_2009 = (por_cobranza_2008 + por_cobranza_2009)
+
+        saldo_clientes_2009 = (total_clientes_2009 - total_entradas_2009)
+        print(f"\nEl saldo de clientes del 2009 es: {saldo_clientes_2009}")
+
+        input("Presione enter para continuar...")
+        borrar()
+
 
     elif respuestaMenu == 3:
         print('Seleccciono Prespuesto de Producción ')
@@ -114,4 +141,4 @@ while respuestaMenu != 13:
         print('Seleccciono Presupuesto Financiero ')
         print(' ')
     
-print('Adios ')
+print('Adiós ')
